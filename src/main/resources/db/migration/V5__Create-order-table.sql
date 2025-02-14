3989dd2cd143600562404e934ad0c2e61590dedb
Gustavolskw <gustavolschmidt13@gmail.com>
2025-02-14 13:15:59 -0300

    fix:references to pg database on Order and migrations

--- src/main/resources/db/migration/V5__Create-order-table.sql
+++ src/main/resources/db/migration/V5__Create-order-table.sql
@@ -3,7 +3,7 @@ CREATE TABLE orders (
                         consumer_name VARCHAR(150) NOT NULL,
                         table_number INT NOT NULL,
                         guide BIGINT NOT NULL,
-                        status SMALLINT NOT NULL,
+                        status BOOLEAN NOT NULL DEFAULT TRUE,
                         created_at TIMESTAMP NOT NULL DEFAULT NOW(),
                         updated_at TIMESTAMP,
                         CONSTRAINT fk_guider FOREIGN KEY(guide) REFERENCES users(id)
